d12e1ae754dba66cee4b543ed3cd8603f63765ae
Tom <tom.edinburgh@ndm.ox.ac.uk>
2025-12-24 10:32:22 +0000

    check field names, f-strings

--- isaricanalytics/data/core.py
+++ isaricanalytics/data/core.py
@@ -11,13 +11,13 @@ The `IsaricData` class has methods to:
 - Add and remove variables.
 """
 
-__author__ = "Tom Edinburgh"
-
 import json
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional
 
 import pandas as pd
+
+from isaricanalytics.utils import sanitise_string
 
 
 @dataclass
@@ -33,6 +33,9 @@ class IsaricData:
     events: Optional[Dict[str, pd.DataFrame]]
 
     def __post_init__(self) -> None:
+        self.validate()
+
+    def validate(self) -> None:
         """Validation of all data."""
         self.validate_metadata()
         self.validate_data_dictionary()
@@ -57,6 +60,15 @@ class IsaricData:
         if not isinstance(self.data_dictionary, pd.DataFrame):
             raise TypeError("data_dictionary must be a pandas DataFrame")
 
+        field_name_check = self.data_dictionary['field_name'].apply(
+            lambda x: x != sanitise_string(x) or not str(x)[0].isalpha()
+        )
+        if field_name_check.any():
+            field_names = ", ".join(
+                self.data_dictionary.loc[field_name_check, 'field_name']
+            )
+            raise ValueError(f"field_names ({field_names}) don't conform to schema")
+
     def validate_table(self, table_name: str) -> None:
         """Validation of a named table.
 
@@ -65,7 +77,7 @@ class IsaricData:
         """
         table = getattr(self, table_name)
         if not isinstance(table, pd.DataFrame):
-            raise TypeError("%s must be a pandas DataFrame", table_name)
+            raise TypeError(f"{table_name} must be a pandas DataFrame")
 
     def describe(self) -> str:
         """Print a summary of the instance. TODO."""
@@ -85,8 +97,13 @@ class IsaricData:
     def get_fields(self, field_names: List[str], table_name: str) -> pd.DataFrame:
         return
 
-    def get_type(self, field_type: str, table_name: str) -> pd.DataFrame:
-        return
+    def get_field_names(self, field_types: List[str], table_names: List[str]) -> List[str]:
+        """Get field names by field types and/or table names. TODO properly"""
+        mask = (
+            self.data_dictionary["field_type"].isin(field_types)
+            & self.data_dictionary["table_name"].isin(table_names)
+        )
+        return self.data_dictionary.loc[mask, "field_name"].tolist()
 
     def add_derived_field(
         self, field_name: str, table_name: str, **kwargs
