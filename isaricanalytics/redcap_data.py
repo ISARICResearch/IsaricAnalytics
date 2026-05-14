5eddade47fc0361a476cac09bf280878b428957b
EstebanGarciaG <jegargax@gmail.com>
2026-05-14 13:26:26 +0100

    linting fixes

--- isaricanalytics/redcap_data.py
+++ isaricanalytics/redcap_data.py
@@ -1409,11 +1409,14 @@ def get_df_map(
         dictionary, and the quality report.
     """  # noqa : E501
     df_map = data.copy()
-    forms = ["presentation", "daily", "outcome",'inclusion_criteria', 'consent']
+    forms = ["presentation", "daily", "outcome", "inclusion_criteria", "consent"]
     columns = dictionary.loc[dictionary["form_name"].isin(forms), "field_name"].tolist()
     columns = [col for col in columns if col in df_map.columns]
     ind = data["form_name"].apply(
-        lambda x: any(y in x.split(",") for y in ["presentation", "outcome",'inclusion_criteria', 'consent'])
+        lambda x: any(
+            y in x.split(",")
+            for y in ["presentation", "outcome", "inclusion_criteria", "consent"]
+        )
     )
     df_map = df_map.reset_index(drop=True)
     ind = ind.reset_index(drop=True) if hasattr(ind, "reset_index") else ind
