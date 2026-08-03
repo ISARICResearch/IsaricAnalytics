cf817b61319a27d6d74696216ec400842783c84c
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-08-03 14:39:59 +0100

    feat: add ISARIC data schema template lib

---
+++ isaricanalytics/isaric_data_schema.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+__all__ = []
+
+
+# -- IMPORTS --
+
+# -- Standard libraries --
+
+
+# -- 3rd party libraries --
+
+# -- Internal libraries --
+
+# PLACEHOLDER
