333976929ff9a9e3632b47a49f89ba4cf41030f8
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-08-06 15:21:46 +0100

    docs: complete doctests for `utils`

--- isaricanalytics/utils.py
+++ isaricanalytics/utils.py
@@ -31,7 +31,14 @@ def strip_html(value: typing.Any) -> str | typing.Any:
     str, typing.Any
         Either a string stripped of all HTML elements, or the original non-
         string value.
-    """
+
+    Examples
+    --------
+    >>> strip_html("<b><i>A value</i></b>")
+    'A value'
+    >>> strip_html("<p>This is a paragraph.</p>")
+    'This is a paragraph.'
+    """  # noqa : E501
     if isinstance(value, str):
         return re.sub(r"<.*?>", "", value)
 
@@ -55,7 +62,14 @@ def strip_nonstandard_unicode_chars(value: typing.Any) -> str | typing.Any:
     str, typing.Any
         Either a string stripped of all non-standard Unicode characters, or the
         original non- string value.
-    """
+
+    Examples
+    --------
+    >>> strip_nonstandard_unicode_chars("A value with a special Unicode ↳ character")
+    'A value with a special Unicode  character'
+    >>> strip_nonstandard_unicode_chars("<p>This is a paragraph ending with a special Unicode character ↳.</p>")
+    '<p>This is a paragraph ending with a special Unicode character .</p>'
+    """  # noqa : E501
     nonstandard_unicode_chars = "↳"
 
     if isinstance(value, str):
@@ -67,10 +81,13 @@ def strip_nonstandard_unicode_chars(value: typing.Any) -> str | typing.Any:
 def clean_figure_table(figure_table: pandas.DataFrame) -> pandas.DataFrame:
     """:py:class:`pandas.DataFrame` : A cleaned figure table dataframe.
 
-    The cleaning steps are unique to the Plotly graph object table format from
-    which the table CSVs were originally, which contain HTML styling elements
-    and non-standard (non-alphabetic) Unicode characters. The cleaning is the
-    removal of such characters.
+    This function is not intended to be highly generic, but was written with
+    the aim of producing clean dataframes for the plotting functions in the
+    :py:mod:`isaricanalytics.visualisation` library. The cleaning steps are:
+
+    * removal of HTML styling elements
+    * removal of non-standard (non-alphabetic) Unicode characters, currently
+      limited to ``↳``.
 
     Parameters
     ----------
@@ -81,10 +98,41 @@ def clean_figure_table(figure_table: pandas.DataFrame) -> pandas.DataFrame:
     -------
     pandas.DataFrame
         The cleaned figure table.
-    """
-    # The use of `pandas.DataFrame.map` here is not absolutely optimal, as
-    # `map` applies changes across the dataframe element-wise, but is the
-    # safer choice given that the dataframe may contain a number of non-string
-    # columns which cannot be known in advance, while the cleaning steps
-    # currently only apply to string values.
+
+    Examples
+    --------
+    >>> import io, pandas as pd
+    >>> data = pd.read_csv(io.StringIO(
+    ...     '''
+    ...     Variable,All,Discharged,Death,Censored
+    ...     <b>Totals</b>,1000,219,326,455
+    ...     <b><i>COMPLICATIONS</i></b>,,,,
+    ...     <b>Seizure</b> (*),665 (81.7) | 814,148 (80.9) | 183,207 (79.6) | 260,310 (83.6) | 371
+    ...     <b>Focal neurological signs</b> (*),702 (74.8) | 938,156 (75.4) | 207,231 (75.5) | 306,315 (74.1) | 425
+    ...     <b>Encephalitis</b> (*),481 (52.6) | 914,102 (52.0) | 196,161 (53.5) | 301,218 (52.3) | 417
+    ...     <b>Meningitis</b> (*),781 (90.2) | 866,173 (88.7) | 195,252 (91.0) | 277,356 (90.4) | 394
+    ...     <b>Cardiac arrhythmia</b> (*),241 (26.7) | 901,64 (32.0) | 200,70 (24.1) | 291,107 (26.1) | 410
+    ...     '''
+    ... ), skipinitialspace=True)
+    >>> cleaned_data = clean_figure_table(data)
+    >>> cleaned_data
+                           Variable               All        Discharged             Death          Censored
+    0                        Totals              1000               219               326               455
+    1                 COMPLICATIONS               NaN               NaN               NaN               NaN
+    2                   Seizure (*)  665 (81.7) | 814  148 (80.9) | 183  207 (79.6) | 260  310 (83.6) | 371
+    3  Focal neurological signs (*)  702 (74.8) | 938  156 (75.4) | 207  231 (75.5) | 306  315 (74.1) | 425
+    4              Encephalitis (*)  481 (52.6) | 914  102 (52.0) | 196  161 (53.5) | 301  218 (52.3) | 417
+    5                Meningitis (*)  781 (90.2) | 866  173 (88.7) | 195  252 (91.0) | 277  356 (90.4) | 394
+    6        Cardiac arrhythmia (*)  241 (26.7) | 901   64 (32.0) | 200   70 (24.1) | 291  107 (26.1) | 410
+    """  # noqa : E501
     return figure_table.map(strip_html).map(strip_nonstandard_unicode_chars)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    # Doctest the module from the project root using
+    #
+    #     PYTHONPATH="isaricanalytics" python3 -m doctest -v isaricanalytics/utils.py  # noqa : E501
+    #
+    import doctest
+
+    doctest.testmod()
