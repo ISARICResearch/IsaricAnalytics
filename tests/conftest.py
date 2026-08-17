25d4c7f7c7fe854b3317cb6962b13ee7ec003ea5
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-08-17 13:51:51 +0100

    Various changes including use of fixtures and updating relevant tests, and switching to an `src`-style package layout

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,30 @@
-#
-# Define fixtures here.
-#
+# -- IMPORTS --
+
+# -- Standard libraries --
+from pathlib import Path
+
+# -- 3rd party libraries --
+import pandas as pd
+import pytest
+
+# -- Internal libraries --
+
+
+@pytest.fixture(scope="module")
+def countries_filepath() -> pd.DataFrame:
+    return Path(__file__).parent.joinpath("unit", "assets", "countries.csv")
+
+
+@pytest.fixture(scope="module")
+def countries(countries_filepath) -> pd.DataFrame:
+    return pd.read_csv(countries_filepath, encoding="latin-1")
+
+
+@pytest.fixture(scope="module")
+def conversion_table_filepath() -> pd.DataFrame:
+    return Path(__file__).parent.joinpath("unit", "assets", "conversion_table.csv")
+
+
+@pytest.fixture(scope="module")
+def conversion_table(conversion_table_filepath) -> pd.DataFrame:
+    return pd.read_csv(conversion_table_filepath, encoding="latin-1")
