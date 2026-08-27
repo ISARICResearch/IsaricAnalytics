09594e119b5ca743cdccc48ea016426fb87e00ed
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-08-27 11:08:02 +0100

    test: Further work on the ISARIC data schema pipeline + tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,20 +11,57 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def countries_filepath() -> pd.DataFrame:
-    return Path(__file__).parent.joinpath("unit", "assets", "countries.csv")
+def countries() -> pd.DataFrame:
+    fp = Path(__file__).parent.joinpath("unit", "assets", "countries.csv")
+    assert fp.exists()
+    return pd.read_csv(fp, encoding="latin-1")
 
 
 @pytest.fixture(scope="module")
-def countries(countries_filepath) -> pd.DataFrame:
-    return pd.read_csv(countries_filepath, encoding="latin-1")
+def conversion_table() -> pd.DataFrame:
+    fp = Path(__file__).parent.joinpath("unit", "assets", "conversion_table.csv")
+    assert fp.exists()
+    return pd.read_csv(fp, encoding="latin-1")
 
 
 @pytest.fixture(scope="module")
-def conversion_table_filepath() -> pd.DataFrame:
-    return Path(__file__).parent.joinpath("unit", "assets", "conversion_table.csv")
+def isaric_data_schema_example_dataset_filepath() -> Path:
+    return (
+        Path(__file__)
+        .parent.joinpath("unit", "assets", "isaric_data_schema_example_dataset.csv")
+        .resolve()
+    )
 
 
 @pytest.fixture(scope="module")
-def conversion_table(conversion_table_filepath) -> pd.DataFrame:
-    return pd.read_csv(conversion_table_filepath, encoding="latin-1")
+def isaric_data_schema_example_parser_filepath() -> Path:
+    return (
+        Path(__file__)
+        .parent.joinpath("unit", "assets", "isaric_data_schema_example_parser.toml")
+        .resolve()
+    )
+
+
+@pytest.fixture(scope="module")
+def isaric_data_schema_example_core_table_filepath() -> Path:
+    return (
+        Path(__file__)
+        .parent.joinpath("unit", "assets", "isaric_data_schema_example_core_table.csv")
+        .resolve()
+    )
+
+
+@pytest.fixture(scope="module")
+def isaric_data_schema_example_long_table_filepath() -> Path:
+    return (
+        Path(__file__)
+        .parent.joinpath("unit", "assets", "isaric_data_schema_example_long_table.csv")
+        .resolve()
+    )
+
+
+@pytest.fixture(scope="module")
+def arc_data_dictionary_v1_5_0() -> pd.DataFrame:
+    fp = Path(__file__).parent.joinpath("unit", "assets", "arc_v1.5.0.csv").resolve()
+    assert fp.exists()
+    return pd.read_csv(fp)
